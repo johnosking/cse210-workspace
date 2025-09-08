707f1fda582b6fd12ae31b54f1dda4030613830a
johnosking <johnspecial080@gmail.com>
2025-09-08 10:30:38 +0100

    Completion of 1st Core requirement.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.Write("What is your grade percentage? ");
+        string userinput = Console.ReadLine();
+
+        int gradePercentage = int.Parse(userinput);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+    
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+
+        }
+        
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+    }    
 }
