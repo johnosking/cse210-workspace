665125d1316e6bddd153c91853afc0c1d4817d80
johnosking <johnspecial080@gmail.com>
2025-09-09 18:48:20 +0100

    Completion of 1st Core requirement.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        string userNumber = Console.ReadLine();
+        int magic = int.Parse(userNumber);
+    
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+
+        if (guess < magic)
+        {
+            Console.Write("Guess Higher");
+        }
+        else if (guess == magic)
+        {
+            Console.Write("You guessed it");
+        }
+        else
+        {    
+            Console.Write("Guess Lower");
+        }
+
+
     }
 }
