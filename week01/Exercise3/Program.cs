e8aabcdeed5436e4b4e7dcf2c45bfb73fac2bb50
johnosking <johnspecial080@gmail.com>
2025-09-09 19:44:36 +0100

    Completion of 3rd Core requirement

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,26 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        string userNumber = Console.ReadLine();
-        int magic = int.Parse(userNumber);
-    
+        //Console.Write("What is the magic number? ");
+        //string userNumber = Console.ReadLine();
+        //int magic = int.Parse(userNumber);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
 
 
         int guess = -1;
 
-        while (guess != magic)
+        while (guess != number)
         {
             Console.Write("What is your guess? ");
             string userGuess = Console.ReadLine();
             guess = int.Parse(userGuess);
 
-            if (guess < magic)
+            if (guess < number)
             {
                 Console.WriteLine("Guess Higher");
             }
 
-            else if (guess > magic)
+            else if (guess > number)
             {    
                 Console.WriteLine("Guess Lower");
             }
