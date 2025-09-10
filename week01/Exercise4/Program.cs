88abdda2985cbcdef250ce384ed1abe5306153bd
johnosking <johnspecial080@gmail.com>
2025-09-10 12:19:10 +0100

    Completion of 1st Core Requirement.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,31 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumbers = -1;
+
+        while (userNumbers != 0)
+        {
+            Console.Write("Enter a number (enter 0 to quit): ");
+            string userResponse = Console.ReadLine();
+            userNumbers = int.Parse(userResponse);
+
+            if (userNumbers != 0)
+            {
+                numbers.Add(userNumbers);
+            }
+
+        }
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of numbers is {sum}");
     }
 }
