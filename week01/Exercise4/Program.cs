8a6fa5b1fb35025e0403f9e16ea5ef89091e6710
johnosking <johnspecial080@gmail.com>
2025-09-10 16:01:51 +0100

    Completion odf 3rd Core requirement

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -31,5 +31,14 @@ class Program
         int average = sum / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
